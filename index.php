4331a8ff1335bfb03bc02796450034b6054ccc03
Stefan <rpges@outlook.com>
2017-03-09 21:38:20 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="de">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="include\index.css">
+    <link rel="stylesheet" href="css\style.css">
     <title>WortGes Software - Software Partner</title>
   </head>
 <body>

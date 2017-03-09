f6fec63a157224ca09a2eadf3ced27fcba378e5c
Alex <alexander.r.wortmann@gmail.com>
2017-03-09 20:34:36 +0100

    09.03.2017

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="include\index.css">
+    <title>WortGes Software - Software Partner</title>
+  </head>
+<body>
+
+
+
+</body>
+</html>

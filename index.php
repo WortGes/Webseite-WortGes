5b8b25d3177367af5d2faf60a6aeb457930f5fdf
Stefan <rpges@outlook.com>
2017-03-10 15:12:42 +0100

    Updated

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css\style.css">
+    <link rel="stylesheet" href="css\login.css">
     <title>WortGes Software - Software Partner</title>
   </head>
 <body>
--- login.php
+++ login.php
@@ -1,39 +1,45 @@
-<div id="id01" class="modal">
-  <!--<span onclick="document.getElementById( 'id01' ).style.display='none'" class="close" title="Close Modal">x</span>-->
-  <form class="modal-content animate" action="/action_page.php">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById( 'id01' ).style.display='noce'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar.png" alt="Avatar" class="avatar">
+<html>
+  <head>
+    <link rel="stylesheet" href="css/login.css">
+  </head>
+  <body>
+    <div id="id01" class="modal">
+      <form class="modal-content animate" action="/action_page.php">
+        <div class="imgcontainer">
+          <span onclick="document.getElementById( 'id01' ).style.display='noce'" class="close" title="Close Modal">&times;</span>
+          <img src="img_avatar.png" alt="Avatar" class="avatar">
+        </div>
+
+        <div class="container">
+          <label><b>Username</b></label>
+          <input type="text" placeholder="Enter Username" name="uname" required>
+
+          <label><b>Password</b></label>
+          <input type="password" placeholder="Enter Passwort" name="psw" required>
+
+          <button type="submit">Login</button>
+          <input type="checkbox" checked="checked"> Remember me
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+          <button type="button" onclick="document.getElementById( 'id01' ).style.display='none'" class="cancelbtn">Cancel</button>
+          <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+      </form>
     </div>
 
-    <div class="container">
-      <label><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
+    <script>
 
-      <label><b>Password</b></label>
-      <input type="password" placeholder="Enter Passwort" name="psw" required>
+    // Get the modal
+    var modal = document.getElementById( 'id01' );
 
-      <button type="submit">Login</button>
-      <input type="checkbox" checked="checked"> Remember me
-    </div>
+    // When the user clicks anywhere outside of the modal, close it
+    window.onlick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById( 'id01' ).style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
-</div>
-
-<script>
-
-// Get the modal
-var modal = document.getElementById( 'id01' );
-
-// When the user clicks anywhere outside of the modal, close it
-window.onlick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-</script>
+    </script>
+  </body>
+</html>
